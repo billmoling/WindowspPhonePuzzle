e379ae17a80020ec527a07379ea3ca71a876c34a
Ling <panan.songyu@gmail.com>
2012-01-07 23:40:47 +0800

    first commit

---
+++ WindowsPhonePuzzle/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace WindowsPhonePuzzle
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // navigate
+            this.NavigationService.Navigate(new Uri("/PuzzlePage.xaml", UriKind.Relative));
+        }
+    }
+}
